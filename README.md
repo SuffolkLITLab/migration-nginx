c42b85421a2e6f3ab1a9e4804fa5303d6f3c603b
Bryce Willey <bryce.willey@suffolk.edu>
2025-09-19 09:08:02 -0400

    First commit, with Michigan content

---
+++ README.md
@@ -0,0 +1,7 @@
+# Migration Nginx Dockerfile and Contents
+
+## Example usage
+
+```bash
+docker build -t mi-nginx --build-arg ORG_DIR=mi-content .
+```
---
+++ run_nginx.sh
@@ -0,0 +1,2 @@
+# docker run -it --rm -p 8080:80 --name web --mount type=bind,src=./mi-content,dst=/usr/share/nginx/html nginx
+docker run -it --rm -p 80:80 -p 443:443 --name web mi_nginx 
